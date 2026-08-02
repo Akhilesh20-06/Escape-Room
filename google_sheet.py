39aca0cad09903661cd72ddfd47ae1b617d954c5
Akhilesh SJ <akhiloff06@gmail.com>
2026-08-02 12:42:01 +0530

    Use Google Sheets instead of CSV

--- google_sheet.py
+++ google_sheet.py
@@ -1,17 +1,20 @@
 import gspread
-from google.oauth2.service_account import Credentials
 from datetime import datetime
+import os
+import json
+from google.oauth2.service_account import Credentials
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = Credentials.from_service_account_file(
-    "credentials.json",
+creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+
+creds = Credentials.from_service_account_info(
+    creds_dict,
     scopes=SCOPES
 )
-
 client = gspread.authorize(creds)
 
 # Replace with your Google Sheet name
